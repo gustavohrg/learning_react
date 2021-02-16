ea7690aa146117f6b01ba0a8103102f448ae5c5c
gustavohrg <gustavohrg@live.com>
2021-02-16 16:32:12 -0300

    C5 - utilizando props para valores dinâmicos

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ class App extends Component {
     return (
       <div className='App'>
         <h1>Hello!</h1>
-        <Person />
-        <Person />
-        <Person />
+        <Person name='Gustavo' age='23' />
+        <Person name='Felipe' age='17'>
+          Sou irmão do Gustavo
+        </Person>
+        <Person name='Julia' age='20' />
       </div>
     );
   }
