a71b87f333151770b46a72af3c9a7bb8536b82e7
gustavohrg <gustavohrg@live.com>
2021-02-19 14:33:04 -0300

    C10 - Referenciado métodos entre componentes

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ class App extends Component {
     otherState: 'some value',
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Guto';
     // DO THIS INSTEAD
     this.setState({
       persons: [
-        { name: 'Guto', age: 23 },
+        { name: newName, age: 23 },
         { name: 'Felipe', age: 17 },
         { name: 'Carol', age: 24 },
       ],
@@ -30,7 +30,10 @@ class App extends Component {
       <div className='App'>
         <h1>Hello!</h1>
         <p>A paragraph</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        {/* not the most efficient use of passing arguments */}
+        <button onClick={() => this.switchNameHandler('Guto!')}>
+          Switch Name
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -38,6 +41,8 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          // best use of passing arguments
+          click={this.switchNameHandler.bind(this, 'Guto')}
         >
           Sou irmão do Gustavo
         </Person>
