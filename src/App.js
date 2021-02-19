d644b5d5849bb88f005b1d34797ed7ea8181e96d
gustavohrg <gustavohrg@live.com>
2021-02-19 14:44:50 -0300

    C11 - Via dupla de alteração/visualização em tempo real de valores

--- src/App.js
+++ src/App.js
@@ -25,6 +25,16 @@ class App extends Component {
     });
   };
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Guto', age: 23 },
+        { name: event.target.value, age: 17 },
+        { name: 'Carol', age: 24 },
+      ],
+    });
+  };
+
   render() {
     return (
       <div className='App'>
@@ -43,6 +53,7 @@ class App extends Component {
           age={this.state.persons[1].age}
           // best use of passing arguments
           click={this.switchNameHandler.bind(this, 'Guto')}
+          changed={this.nameChangedHandler}
         >
           Sou irmão do Gustavo
         </Person>
