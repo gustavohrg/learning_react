f8b656b324b6b280c174f4007d830b3c1197a337
gustavohrg <gustavohrg@live.com>
2021-02-24 22:10:15 -0300

    C16 - renderizando listas dinamicamente

--- src/App.js
+++ src/App.js
@@ -55,23 +55,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            // best use of passing arguments
-            click={this.switchNameHandler.bind(this, 'Guto')}
-            changed={this.nameChangedHandler}
-          >
-            Sou irmão do Gustavo
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map((person) => {
+            return <Person name={person.name} age={person.age} />;
+          })}
         </div>
       );
     }
