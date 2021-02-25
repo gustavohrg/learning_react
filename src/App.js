015d62490cb7762ee6f2e7f105e9ebff73d571a6
gustavohrg <gustavohrg@live.com>
2021-02-24 22:38:16 -0300

    C20 - handler em listas flexiveis

--- src/App.js
+++ src/App.js
@@ -13,14 +13,23 @@ class App extends Component {
     showPersons: false,
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Guto', age: 23 },
-        { name: event.target.value, age: 17 },
-        { name: 'Carol', age: 24 },
-      ],
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
     });
+
+    // Alternative to object copy
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    const person = {
+      ...this.state.persons[personIndex],
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
   deletePersonHandler = (personIndex) => {
@@ -57,6 +66,7 @@ class App extends Component {
                 name={person.name}
                 age={person.age}
                 key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
